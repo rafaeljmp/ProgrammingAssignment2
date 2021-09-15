f2037f499ab21938ebdc72f9150b0a0496e3f77d
rafaeljmp <88784067+rafaeljmp@users.noreply.github.com>
2021-09-15 11:39:11 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Inicialitza la matriu si no existeixen dades
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        z<-NULL
+        set<-function(y){
+                x<-y
+                z<-NULL
 }
-
-
+        get <- function() x
+        setinversa <- function(inverse) z <<- inverse
+        getinversa <- function() z
+        list(set = set,
+             get = get,
+             setinversa = setinversa,
+             getinversa = getinversa)        
+}
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        z <- x$getinversa()
+        if (!is.null(z)) {
+                message("trau les dades de la cache")
+                return(z)
+        }
+        data <- x$get()
+        z <- solve(data, ...)
+        x$setinversa(z)
+        z
 }
+
